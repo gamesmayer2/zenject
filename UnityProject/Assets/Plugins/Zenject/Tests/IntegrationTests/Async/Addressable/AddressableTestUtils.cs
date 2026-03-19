668fce6bce3f882e19e5adcff912a12dd618029f
Carlos Sala Samper <carlos15695@hotmail.com>
2026-03-19 05:12:31 +0200

    fix: addressables

--- UnityProject/Assets/Plugins/Zenject/Tests/IntegrationTests/Async/Addressable/AddressableTestUtils.cs
+++ UnityProject/Assets/Plugins/Zenject/Tests/IntegrationTests/Async/Addressable/AddressableTestUtils.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
@@ -13,12 +14,18 @@ namespace Zenject.Tests.IntegrationTests.Async.Addressable
     {
         public static IEnumerator GetAddressableReference(string key, Action<AssetReferenceGameObject> onFound)
         {
+            if (!HasAddressablesRuntimeData())
+            {
+                Assert.Inconclusive("Addressables runtime data is missing. Build Addressables Player Content before running this test.");
+                yield break;
+            }
+
             AsyncOperationHandle<IList<IResourceLocation>> locationsHandle;
             try
             {
                 locationsHandle = Addressables.LoadResourceLocationsAsync(key);
             }
-            catch (Exception e)
+            catch (Exception)
             {
                 Assert.Inconclusive("You need to set TestAddressablePrefab key to run this test");
                 yield break;
@@ -43,6 +50,20 @@ namespace Zenject.Tests.IntegrationTests.Async.Addressable
             }
 
             onFound?.Invoke(new AssetReferenceGameObject(resourceLocation.PrimaryKey));
+        }
+
+        private static bool HasAddressablesRuntimeData()
+        {
+            const string addressablesDataRoot = "Library/com.unity.addressables/aa";
+
+            if (!Directory.Exists(addressablesDataRoot))
+            {
+                return false;
+            }
+
+            // Any built platform runtime settings file is enough to run these editor-side tests.
+            string[] settingsFiles = Directory.GetFiles(addressablesDataRoot, "settings.json", SearchOption.AllDirectories);
+            return settingsFiles.Length > 0;
         }
     }
 }
